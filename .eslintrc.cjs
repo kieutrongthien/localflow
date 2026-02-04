4604aaef7906814a51d95116cf884c0d57c46960
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2026-02-05 03:48:43 +0700

    Chore: remove cached

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@electron-toolkit/eslint-config/vue'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json']
-  }
-}
--- src/renderer/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    localflow: {
-      ping: () => string
-    }
-  }
-}
-
-export {}
